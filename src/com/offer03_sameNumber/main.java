e131cbcf7f5ee5f11a030a6512c08cbf31d457bd
xmc473@jsti.com <xmc473@jsti.com>
2023-08-04 09:36:43 +0800

    剑指 Offer 03. 数组中重复的数字

---
+++ src/com/offer03_sameNumber/main.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public int findRepeatNumber(int[] nums) {
+
+        if (nums.length == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length;j++) {
+                if (nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+
+
+        }
+
+        return 1;
+    }
+
+}
