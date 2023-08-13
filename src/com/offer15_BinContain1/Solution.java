059170d062b9f2785b3e1fa488826b54c8cb57cc
xmc473@jsti.com <xmc473@jsti.com>
2023-08-13 18:21:18 +0800

    剑指 Offer 15. 二进制中1的个数

---
+++ src/com/offer15_BinContain1/Solution.java
@@ -0,0 +1,63 @@
+package com.offer15_BinContain1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int hammingWeight1(int n) throws IOException {
+
+//        n = 128 (控制台输入 00000000000000000000000000001011
+        int count = 0;
+
+        String str ;
+      
+//        str1= new Integer(n).byteValue();
+           str=Integer.toBinaryString(n);
+
+
+
+        char[] arr = str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (1 == arr[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
+
+
+    public int hammingWeight(int n) throws IOException {
+
+//        n = 128 (控制台输入 00000000000000000000000000001011
+        int count = 0;
+
+        String str ;
+
+//        str1= new Integer(n).byteValue();
+        str=Integer.toBinaryString(n);
+
+
+        System.out.println(str);
+        char[] arr = str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (49 == arr[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
